7d81b8b22ab9c8ca63b1540909b26cb9bd9e7517
unknown <rajarshibhatt873@gmail.com>
2021-09-30 19:32:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,141 +1,169 @@
 //jshint esversion:6
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import 'C:/Users/Rajarshi/Desktop/FullStack/figma/src/App.css';
-import 'C:/Users/Rajarshi/Desktop/FullStack/figma/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import * as firebase from '../node_modules/firebase/app';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Button, Nav, Container, NavbarBrand, Navbar, NavLink, } from 'react-bootstrap';
 import img1 from './img1.jpeg';
 import img2 from './img2.jpeg';
 import img3 from './img3.jpeg';
-import vid1 from './Animation S.mp4'
+import vid1 from './Animation NEW.mp4'
+import firebaseConfig from './config';
+require('firebase/auth');
+require('firebase/database');
 
-function App() {
-  return (
-    <>
-      <div>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home"><i>Abc</i>Bank</Navbar.Brand>
-            <Navbar.Toggle aria-controls="-navbar-nav" className="order-md-1 order-0" />
-            <Nav className="topnav-right">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
+firebase.initializeApp(firebaseConfig);
 
-      <div className="home">
-        <table>
-          <tr>
-            <td>
-              <h5>SIMPLE, SECURE, COMMISSION FREE</h5>
-              <h1>Your Tool For real-world <br /> tasks</h1>
-              <h6 className="hh6">Abc Bank is a banking App with zero balance savings <br /> acoount. It helps you get better with your money - <br /> one deligthful feature at the same time</h6>
-              <div className="bt1">
-                <Button className="bt1" variant="light"><b>Get Early Access</b></Button>
-              </div>
-            </td>
-            <td>
-              <video preload="auto" className="vid" autoPlay="true" muted playsInline>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: [] }
+  }
+
+  componentDidMount() {
+    const myItem1 = firebase.database().ref("pageDesc");
+    let llist = []
+    myItem1.on("value", datasnap => {
+      var obj = datasnap.val();
+      console.log(obj);
+      for (const property in obj) {
+        llist.push(obj[property])
+      }
+      console.log(llist);
+      this.setState({ list: llist });
+      console.log(this.state.list);
+    });
+  }
+
+  render() {
+    var arr = [];
+    this.state.list.map(data => {
+      arr.push(data);
+    })
+    console.log(arr);
+    return (
+      <>
+        <div className="home">
+          <div className="b">
+            <video preload="auto" className="vid" autoPlay="true" muted playsInline>
               <source src={vid1} type="video/mp4" />
-              </video>
-            </td>
-          </tr>
-        </table>
-      </div>
+            </video>
+          </div>
+          <div className="a">
+            <h5 style={{color:arr[6]}}>{arr[0]}</h5>
+            <h1 className="head1">{arr[1]}</h1>
+            <h6 className="hh6" style={{color:arr[6]}}>{arr[0]}</h6>
+            <div className="bt1">
+              <Button className="bt1" variant="light"><b>Get Early Access</b></Button>
+            </div>
+          </div>
+        </div>
 
-      <div className="page2">
-        <h1 className="hh1">Banking That Works Around You</h1>
-        <table>
-          <tr>
-            <td className="cell">
-              <h2 className="hh11">Gamified Experience</h2>
-              <h4 className="hh4">GET MORE BITCOIN THAN DCA</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-              <h4 className="hh4">INVEST AT YOUR OWN PACE</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </td>
-            <td >
-              <img className="img1" src={img1} alt="none" />
-            </td>
-          </tr>
-        </table>
-      </div>
+        <div>
+          <Navbar className="nav-color" fixed="top" bg="black" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home"><h1><i>Abc</i>Bank</h1></Navbar.Brand>
+              <Navbar.Toggle aria-controls="-navbar-nav" className="order-md-1 order-0" />
+              <Nav className="topnav-right">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </div>
 
-      <div className="page3">
-        <table>
-          <tr>
-            <td>
-              <img className="img2" src={img2} alt="none" />
-            </td>
-            <td className="cell1">
-              <h2 className="hh11">AI-Based Assistant</h2>
-              <h4 className="hh4">AI-BASED ASSISTANT TO EASE BANKING</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-              <h4 className="hh4">AI-BASED ASSISTANT TO EASE BANKING</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </td>
-          </tr>
-        </table>
-      </div>
+        <div className="page2">
+          <h1 className="hh1">{arr[13]}</h1>
+          <div className="grad1">
+          </div>
+          <table>
+            <tr>
+              <td className="cell">
+                <h2 className="hh11">{arr[13]}</h2>
+                <h4 className="hh4">{arr[14]}</h4>
+                <p style={{color:arr[8]}}>{arr[12]}</p>
+                <h4 className="hh4">{arr[15]}</h4>
+                <p style={{color:arr[8]}}>{arr[12]}</p>
+              </td>
+              <td >
+                <img className="img1" src={img1} alt="none" />
+              </td>
+            </tr>
+          </table>
+        </div>
 
-      <div className="page2">   //Actually page 3
-        <table>
-          <tr>
-            <td className="cell">
-              <h2 className="hh11">We Nudge You To Save More</h2>
-              <h4 className="hh4">WE NUDGE YOU TO SAVE MORE</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-              <h4 className="hh4">WE NUDGE YOU TO SAVE MORE</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </td>
-            <td >
-              <img className="img3" src={img3} alt="none" />
-            </td>
-          </tr>
-        </table>
-      </div>
+        <div className="page3">
+          <table>
+            <tr>
+              <td>
+                <img className="img2" src={img2} alt="none" />
+              </td>
+              <td className="cell1">
+                <h2 className="hh11">{arr[17]}</h2>
+                <h4 className="hh4">{arr[18]}</h4>
+                <p style={{color:arr[10]}}>{arr[16]}</p>
+                <h4 className="hh4">{arr[19]}</h4>
+                <p style={{color:arr[10]}}>{arr[16]}</p>
+              </td>
+            </tr>
+          </table>
+        </div>
 
-      <div className="page4">
-        <h1 className="Team">Our Team's Background</h1>
-        <table className="tt">
-          <tr>
-            <td>
-              <img className="sponsor" src="https://www.logo.wine/a/logo/Apple_Pay/Apple_Pay-Logo.wine.svg" />
-            </td>
-            <td>
-              <img className="sponsor" src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" />
-            </td>
-            <td>
-              <img className="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png" />
-            </td>
-            <td>
-              <img className="sponsor" src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" />
-            </td>
-          </tr>
-        </table>
-      </div>
+        <div className="page2">
+          <table>
+            <tr>
+              <td className="cell">
+                <h2 className="hh11">{arr[5]}</h2>
+                <h4 className="hh4">{arr[2]}</h4>
+                <p>{arr[4]}</p>
+                <h4 className="hh4">{arr[3]}</h4>
+                <p>{arr[4]}</p>
+              </td>
+              <td >
+                <img className="img3" src={img3} alt="none" />
+              </td>
+            </tr>
+          </table>
+        </div>
 
-      <div>
-        <Navbar sticky="bottom" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Toggle aria-controls="-navbar-nav" className="order-md-1 order-0" />
-            <Nav className="topnav-left">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#home">About</Nav.Link>
-              <Nav.Link href="#features">Blogs</Nav.Link>
-              <Nav.Link href="#pricing">Privacy Policy</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
+        <div className="page4">
+          <h1 className="Team">Our Team's Background</h1>
+          <table className="tt">
+            <tr>
+              <td>
+                <img className="sponsor" src="https://www.logo.wine/a/logo/Apple_Pay/Apple_Pay-Logo.wine.svg" />
+              </td>
+              <td>
+                <img className="sponsor" src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" />
+              </td>
+              <td>
+                <img className="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png" />
+              </td>
+              <td>
+                <img className="sponsor" src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" />
+              </td>
+            </tr>
+          </table>
+        </div>
 
-    </>
-  );
+        <div>
+          <Navbar className="nav-color" sticky="bottom" bg="black" variant="dark">
+            <Container>
+              <Navbar.Toggle aria-controls="-navbar-nav" className="order-md-1 order-0" />
+              <Nav className="topnav-left">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#home">About</Nav.Link>
+                <Nav.Link href="#features">Blogs</Nav.Link>
+                <Nav.Link href="#pricing">Privacy Policy</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBsjY5pPTNTX_FvTccD_873TzCVtTqPuig",
+  authDomain: "figma-37de5.firebaseapp.com",
+  databaseURL: "https://figma-37de5-default-rtdb.firebaseio.com",
+  projectId: "figma-37de5",
+  storageBucket: "figma-37de5.appspot.com",
+  messagingSenderId: "137529366172",
+  appId: "1:137529366172:web:8748801a15fa0c4d73ff59",
+  measurementId: "G-GD5HMHMC6E"
+};
+
+export default firebaseConfig;
