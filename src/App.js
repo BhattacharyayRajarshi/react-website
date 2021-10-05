aa4311c02f6d2e539c926d90f66bdd2213748018
unknown <rajarshibhatt873@gmail.com>
2021-10-05 14:00:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ class App extends React.Component {
             </video>
           </div>
           <div className="a">
-            <h5 style={{color:arr[6]}}>{arr[0]}</h5>
+            <h5 style={{ color: arr[6] }}>{arr[0]}</h5>
             <h1 className="head1">{arr[1]}</h1>
-            <h6 className="hh6" style={{color:arr[6]}}>{arr[0]}</h6>
+            <h6 className="hh6" style={{ color: arr[6] }}>{arr[0]}</h6>
             <div className="bt1">
               <Button className="bt1" variant="light"><b>Get Early Access</b></Button>
             </div>
@@ -65,11 +65,13 @@ class App extends React.Component {
             <Container>
               <Navbar.Brand href="#home"><h1><i>Abc</i>Bank</h1></Navbar.Brand>
               <Navbar.Toggle aria-controls="-navbar-nav" className="order-md-1 order-0" />
-              <Nav className="topnav-right">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
+              <Navbar.Collapse className="justify-content-end">
+                <Nav className="topnav-right">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </div>
@@ -78,74 +80,40 @@ class App extends React.Component {
           <h1 className="hh1">{arr[13]}</h1>
           <div className="grad1">
           </div>
-          <table>
-            <tr>
-              <td className="cell">
-                <h2 className="hh11">{arr[13]}</h2>
-                <h4 className="hh4">{arr[14]}</h4>
-                <p style={{color:arr[8]}}>{arr[12]}</p>
-                <h4 className="hh4">{arr[15]}</h4>
-                <p style={{color:arr[8]}}>{arr[12]}</p>
-              </td>
-              <td >
-                <img className="img1" src={img1} alt="none" />
-              </td>
-            </tr>
-          </table>
+          <h2 className="hh11">{arr[13]}</h2>
+          <h4 className="hh4">{arr[14]}</h4>
+          <p style={{ color: arr[8] }}>{arr[12]}</p>
+          <h4 className="hh4">{arr[15]}</h4>
+          <p style={{ color: arr[8] }}>{arr[12]}</p>
+          <img className="img1" src={img1} alt="none" />
         </div>
 
         <div className="page3">
-          <table>
-            <tr>
-              <td>
-                <img className="img2" src={img2} alt="none" />
-              </td>
-              <td className="cell1">
-                <h2 className="hh11">{arr[17]}</h2>
-                <h4 className="hh4">{arr[18]}</h4>
-                <p style={{color:arr[10]}}>{arr[16]}</p>
-                <h4 className="hh4">{arr[19]}</h4>
-                <p style={{color:arr[10]}}>{arr[16]}</p>
-              </td>
-            </tr>
-          </table>
+          <h2 className="hh11">{arr[17]}</h2>
+          <h4 className="hh4">{arr[18]}</h4>
+          <p style={{ color: arr[10] }}>{arr[16]}</p>
+          <h4 className="hh4">{arr[19]}</h4>
+          <p style={{ color: arr[10] }}>{arr[16]}</p>
+          <img className="img2" src={img2} alt="none" />
         </div>
 
         <div className="page2">
-          <table>
-            <tr>
-              <td className="cell">
-                <h2 className="hh11">{arr[5]}</h2>
-                <h4 className="hh4">{arr[2]}</h4>
-                <p>{arr[4]}</p>
-                <h4 className="hh4">{arr[3]}</h4>
-                <p>{arr[4]}</p>
-              </td>
-              <td >
-                <img className="img3" src={img3} alt="none" />
-              </td>
-            </tr>
-          </table>
+          <h2 className="hh11">{arr[5]}</h2>
+          <h4 className="hh4">{arr[2]}</h4>
+          <p>{arr[4]}</p>
+          <h4 className="hh4">{arr[3]}</h4>
+          <p>{arr[4]}</p>
+          <img className="img3" src={img3} alt="none" />
         </div>
 
         <div className="page4">
-          <h1 className="Team">Our Team's Background</h1>
-          <table className="tt">
-            <tr>
-              <td>
-                <img className="sponsor" src="https://www.logo.wine/a/logo/Apple_Pay/Apple_Pay-Logo.wine.svg" />
-              </td>
-              <td>
-                <img className="sponsor" src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" />
-              </td>
-              <td>
-                <img className="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png" />
-              </td>
-              <td>
-                <img className="sponsor" src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" />
-              </td>
-            </tr>
-          </table>
+          <h1 className="Team">Our Team's Background <br /></h1>
+          <div className="frnds">
+            <img className="sponsor" src="https://www.logo.wine/a/logo/Apple_Pay/Apple_Pay-Logo.wine.svg" />
+            <img className="sponsor" src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" />
+            <img className="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png" />
+            <img className="sponsor" src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" />
+          </div>
         </div>
 
         <div>
